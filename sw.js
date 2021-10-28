6a1272f774b0362a09f2c632a15d1aea34c3014f
colonnam <colonna_m15@yahoo.com>
2021-10-28 13:53:25 +0200

    a

--- sw.js
+++ sw.js
@@ -10,11 +10,10 @@ const assets=[
     './css/materialize.min.css',
     './img/dish.png',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
-    'https://fonts.gstatic.com/s/materialicons/v111/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+    'https://fonts.gstatic.com/s/materialicons/v111/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2' 
 ]
 
 self.addEventListener('install',evt=>{
-    console.log('installé') 
     evt.waitUntil(
         caches.open(cacheName).then(cache =>{
             cache.addAll(assets)
